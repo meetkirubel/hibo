e89822858b56e0653e6c46f365130b48061bc979
Bantamlak <neoluka315@gmail.com>
2025-04-11 12:40:45 +0300

    email plugin configured

--- admin/config/plugins.ts
+++ admin/config/plugins.ts
@@ -1,1 +1,43 @@
-export default () => ({});
+export default ({ env }) => ({
+  // Allow additional field addition in the registration form
+  'users-permissions': {
+    config: {
+      email_confirmation: true,
+      email_confirmation_redirection: 'https://www.upwork.com',
+      jwt: {
+        expiresIn: '30d',
+      },
+      register: {
+        // Add the fields you want to accept here
+        allowedFields: [
+          'firstName',
+          'lastName',
+          'username',
+          'email',
+          'phoneNumber',
+          'password',
+        ],
+      },
+    },
+  },
+
+  // Email provider
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: env('ADDRESS_FROM'),
+        defaultReplyTo: env('ADDRESS_REPLY'),
+      },
+    },
+  },
+});
