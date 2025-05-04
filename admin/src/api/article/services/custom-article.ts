46e6d1b46272a8ad92a86219679efc1df5cc3b5e
Bantamlak <neoluka315@gmail.com>
2025-05-04 15:40:32 +0300

    article's service is updated

--- admin/src/api/article/services/custom-article.ts
+++ admin/src/api/article/services/custom-article.ts
@@ -15,7 +15,7 @@ export default {
       filters.category = query.category;
     }
     if (query.subcategory) {
-      filters.subcategory = query.subcategory;
+      filters.sub_category = query.subcategory;
     }
     if (query.locale) {
       filters.locale = query.locale;
@@ -50,6 +50,15 @@ export default {
         sort: { createdAt: 'desc' },
         populate: {
           ...query?.populate,
+          author: { fields: ['firstname', 'lastname'] },
+          admin_profile: {
+            fields: ['full_name'],
+            populate: {
+              profile: {
+                fields: ['formats'],
+              },
+            },
+          },
         },
       }),
       strapi.documents('api::article.article').count({ filters }),
@@ -83,27 +92,90 @@ export default {
           total,
         },
       },
-      data: articles.map(({ content, ...rest }) => ({
-        ...rest,
-        likedByCurrentUser: likedArticleIds.has(rest.documentId),
-      })),
+      data: articles.map((article) => {
+        const { content, admin_profile, ...rest } = article as any;
+
+        return {
+          ...rest,
+          likedByCurrentUser: likedArticleIds.has(rest.documentId),
+          admin_profile: {
+            documentId: admin_profile?.documentId,
+            full_name: admin_profile?.full_name,
+            profile_image_url:
+              admin_profile?.profile?.formats?.small?.url ?? null,
+          },
+        };
+      }),
     };
   },
 
   //   Find by slug
-  async getBySlug(slug: string, query: any) {
+  async getBySlug(ctx, userId, slug: string) {
     const result = await strapi.documents('api::article.article').findMany({
-      filters: { slug },
+      filters: { slug, is_premium: true },
       limit: 1,
       populate: {
-        image: { fields: 'url' },
-        category: { fields: 'name' },
+        image: { fields: 'formats' },
+        category: { fields: ['name', 'slug'] },
         sub_category: { fields: 'name' },
         author: { fields: ['firstname', 'lastname'] },
+        admin_profile: {
+          fields: ['full_name'],
+          populate: {
+            profile: {
+              fields: ['formats'],
+            },
+          },
+        },
       },
     });
 
-    return result[0] || null;
+    if (!result || !result[0]) {
+      return ctx.notFound('Article not found');
+    }
+
+    // Get likes for this article
+    const like = await strapi.documents('api::like.like').findMany({
+      filters: {
+        user: { documentId: userId },
+        article: { documentId: result[0].documentId },
+      },
+      populate: {
+        article: {
+          fields: 'id',
+        },
+      },
+    });
+
+    const { content, admin_profile, ...rest } = result[0] as any;
+    if (!ctx.state?.user?.is_subscribed) {
+      return {
+        data: {
+          ...rest,
+          likedByCurrentUser: like.length > 0 ? true : false,
+          author_profile: {
+            documentId: admin_profile?.documentId,
+            full_name: admin_profile?.full_name,
+            profile_image_url:
+              admin_profile?.profile?.formats?.small?.url ?? null,
+          },
+        },
+      };
+    }
+
+    return {
+      data: {
+        ...rest,
+        content,
+        likedByCurrentUser: like.length > 0 ? true : false,
+        author_profile: {
+          documentId: admin_profile?.documentId,
+          full_name: admin_profile?.full_name,
+          profile_image_url:
+            admin_profile?.profile?.formats?.small?.url ?? null,
+        },
+      },
+    };
   },
 
   // Bookmark service
